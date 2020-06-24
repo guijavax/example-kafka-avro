061a1e1d98a9e753c28d0b6c31e3a5a11e6e30ee
Guilherme Carneiro Borges <gcarneib@everis.com>
2020-06-24 14:57:23 -0300

    commit das alteracoes

---
+++ producer/src/test/kotlin/com/example/api/producer/ProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.api.producer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
